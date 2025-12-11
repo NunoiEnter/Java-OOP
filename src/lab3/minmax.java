341b6da13d13ef60cbf8d57917b89b631e281cf9
Monica Seniorious <141556564+NunoiEnter@users.noreply.github.com>
2025-12-12 01:19:39 +0700

    fix minmax

--- src/lab3/minmax.java
+++ src/lab3/minmax.java
@@ -1,36 +1,32 @@
 import java.util.Scanner;
 
-public class minmax {
+public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int max = 0;
-        int min = 0;
-        boolean first = true;
+        System.out.print("Enter a number (-1 to stop): ");
+        int n = sc.nextInt();
 
-        int n = 0;
+        if (n == -1) {
+            System.out.println("No numbers were entered.");
+            return;
+        }
 
-        while (n != -1) {
+        int max = n;
+        int min = n;
+
+        while (true) {
             System.out.print("Enter a number (-1 to stop): ");
             n = sc.nextInt();
 
             if (n == -1) break;
 
-            if (first) {
-                max = n;
-                min = n;
-                first = false;
-            } else {
-                if (n > max) max = n;
-                if (n < min) min = n;
-            }
+            if (n > max) max = n;
+            if (n < min) min = n;
         }
-        if (first) {
-            System.out.println("No numbers were entered.");
-        } else {
-            System.out.println("Maximum = " + max);
-            System.out.println("Minimum = " + min);
-        }
+
+        System.out.println("Maximum = " + max);
+        System.out.println("Minimum = " + min);
     }
 }
