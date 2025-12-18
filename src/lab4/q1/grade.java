a6e6230761a8408c3f4f3deeeb8f67fa572cbc3c
Monica Seniorious <141556564+NunoiEnter@users.noreply.github.com>
2025-12-18 22:07:15 +0700

    added lab4

---
+++ src/lab4/q1/grade.java
@@ -0,0 +1,19 @@
+package q1;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author monica
+ */
+
+public class grade {
+    public static void main(String[] args) {
+        Studentgrade s = new Studentgrade();
+        s.mScore = 80;
+        s.fScore = 80;
+        s.showGrade();
+    }
+}
